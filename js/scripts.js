d1f4ca362352752a8dae27ffbaa315b40efd61d5
Michael Mckeever <mckeever02@gmail.com>
2017-07-27 19:27:49 +0100

    All pages now built

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,70 @@
 $(function() {
-  var iframe = document.getElementById('welcomeVideo');
-  var player = $f(iframe);
-  $('.playFilm').on("click", function(ev) {
-    $('#videoModal').addClass('db');
-    $('#welcomeVideo')[0].src += "&autoplay=1";
-    ev.preventDefault();
-  })
-  $('.close-modal').on("click", function() {
-    $('#videoModal').removeClass('db');
-    player.api("pause");
-  })
+  $('.slider').slick({
+    lazyLoad: 'ondemand',
+    centerMode: true,
+    slidesToShow: 1,
+    centerPadding: '300px',
+    slidesToScroll: 1,
+    appendArrows: $('.arrows-container'),
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1520,
+        settings: {
+          centerPadding: '200px',
+        }
+      },
+      {
+        breakpoint: 1340,
+        settings: {
+          centerPadding: '150px',
+        }
+      },
+      {
+        breakpoint: 1220,
+        settings: {
+          centerPadding: '100px',
+        }
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          centerMode: false,
+          centerPadding: '0',
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $('.slider-dots').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+  });
+
+  $('.tabgroup > div').hide();
+  $('.tabgroup > div:first-of-type').show();
+  $('.tabs a').click(function(e){
+    e.preventDefault();
+      var $this = $(this),
+          tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+          others = $this.closest('li').siblings().children('a'),
+          target = $this.attr('href');
+      others.removeClass('tab-active');
+      $this.addClass('tab-active');
+      $(tabgroup).children('div').hide();
+      $(target).show();
+  });
 });
